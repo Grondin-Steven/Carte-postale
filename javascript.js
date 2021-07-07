7292d45a026556d442d34200564d334af5dae94c
Grd Steven <85608576+Grondin-Steven@users.noreply.github.com>
2021-07-07 16:34:40 +0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function recapitul() {
+    var add1 = document.getElementById("add1").value;
+    var add2 = document.getElementById("add2").value;
+    var add3 = document.getElementById("add3").value;
+    alert("Adresse complète" +" " +add1 +" " +add2 +" " +add3);
+}
+
